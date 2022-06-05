f18a354a618314a91703880be281c8668260b4b6
tim-graupe <tgraupe90@gmail.com>
2022-06-05 14:25:15 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { ReactDOM } from "react";
+import "./style.css";
+class App extends Component {
+  constructor() {
+    super();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1 className="personal">Personal Information</h1>
+
+        <form>
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" id="firstName" required />
+
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" id="lastName" required />
+
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" required />
+
+          <label htmlFor="city">City</label>
+          <input type="text" id="city" required />
+
+          <label htmlFor="state">State</label>
+          <input type="text" id="state" required />
+
+          <label htmlFor="zip">ZIP</label>
+          <input type="text" id="zip" pattern="[0-9]{5}" required/>
+
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" required/>
+
+          <label htmlFor="phone">Phone number</label>
+
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            pattern="[0-9]{10}"
+            required
+          />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
